c5e0386e2e23e2378921ec081b4cf1aecbffbdc3
sekharl.icld@gmail.com <sekharl.icld@gmail.com>
2023-10-07 02:33:42 +0000

    developed the reports products and customers

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,9 +1,23 @@
 select 
 --from raw orders table
-order_id,
-orderdate,
-shipmode,
-ordercostprice,
-ordersellingprice,
-ordersellingprice-ordercostprice as order_profit
-from {{ ref('raw_orders') }}
+o.order_id,
+o.orderdate,
+o.shipmode,
+o.ordercostprice,
+o.ordersellingprice,
+o.ordersellingprice-o.ordercostprice as order_profit,
+--from customer table
+c.customer_id,
+c.customername,
+c.segment,
+c.country,
+--from product table
+p.productid,
+p.category,
+p.productname,
+p.subcategory 
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+on (o.customer_id = c.customer_id )
+left join {{ ref('raw_product') }} as p 
+on(o.product_id = p.productid)
